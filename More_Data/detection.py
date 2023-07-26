faf8fa184dd3f4a9a3ca649147aba58817af092e
Achille Saint-Hillier <achillesainthillier@gmail.com>
2023-07-26 13:05:04 -0400

    detection weights

--- More_Data/detection.py
+++ More_Data/detection.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+ # Load a model
+model = YOLO("yolov8m.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data="config.yaml", epochs=100)  # train the model
+model.train(data="config.yaml", epochs=100, workers=0, device=0)  # train the model
+
+
